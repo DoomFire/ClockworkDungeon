610a1bb273d71125a50883d56d49724e2a2b53b7
connery0 <onmymarkburn4321burn@gmail.com>
2014-03-26 18:23:22 +0100

    clockhatch + testbutton
    added clockhatch recipe and changed player.config to also have the
    hatch, blackboard and the poster (the poster doesn't work, so I added my
    testbutton files to /wired/ (lack of time to find the problem) )

--- objects/wired/testButton/testButton.lua
+++ objects/wired/testButton/testButton.lua
@@ -1,18 +1,42 @@
 function init(args)
 
-local RandomNumber =math.random(100)
-				
-				if RandomNumber <= 33.3 then
-				entity.setAnimationState("switchState", "off")
-				
-				
-				elseif RandomNumber<=66.7 then
-				entity.setAnimationState("switchState", "on")
-				
-				else
-				
-				entity.setAnimationState("switchState", "test")
-				
-				
-				end
+	
+ 
+	local RandomNumber =math.random(100)
+					
+	if entity.direction()==1 then				
+			if RandomNumber <= 33.3 then
+				entity.setAnimationState("switchState", "poster1");
+	
+			elseif RandomNumber<=66.7 then
+				entity.setAnimationState("switchState", "poster2");
+				entity.FrameState=2;
+			else
+			
+				entity.setAnimationState("switchState", "poster3");
+				entity.FrameState=3;
+			end 
+			
+	elseif entity.direction()==-1 then
+			if RandomNumber <= 33.3 then
+				entity.setAnimationState("switchState", "Lposter1");
+	
+			elseif RandomNumber<=66.7 then
+				entity.setAnimationState("switchState", "Lposter2");
+				entity.FrameState=2;
+			else
+			
+				entity.setAnimationState("switchState", "Lposter3");
+				entity.FrameState=3;
+			end 
+	
+	
+	
+	
+	
+	end
+
+
+
 end
+
