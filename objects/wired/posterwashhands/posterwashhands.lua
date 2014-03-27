fa1d2d41f694444b7a1b2dd61443dda9e94bacdd
connery0 <onmymarkburn4321burn@gmail.com>
2014-03-27 09:04:58 +0100

    Fixed the posterwashhands
    changed the lua to acutaly use init and a math random, and changed the
    spriteoffset in .object

--- objects/wired/posterwashhands/posterwashhands.lua
+++ objects/wired/posterwashhands/posterwashhands.lua
@@ -1,24 +1,42 @@
-if entity.direction()==1 then
-if RandomNumber <= 33.3 then
-entity.setAnimationState("switchState", "poster1");
+function init(args)
 
-elseif RandomNumber<=66.7 then
-entity.setAnimationState("switchState", "poster2");
-entity.FrameState=2;
-else
+	
+ 
+	local RandomNumber =math.random(100)
+					
+	if entity.direction()==1 then				
+			if RandomNumber <= 33.3 then
+				entity.setAnimationState("switchState", "state1");
+	
+			elseif RandomNumber<=66.7 then
+				entity.setAnimationState("switchState", "state2");
+				entity.FrameState=2;
+			else
+			
+				entity.setAnimationState("switchState", "state3");
+				entity.FrameState=3;
+			end 
+			
+	elseif entity.direction()==-1 then
+			if RandomNumber <= 33.3 then
+				entity.setAnimationState("switchState", "state4");
+	
+			elseif RandomNumber<=66.7 then
+				entity.setAnimationState("switchState", "state5");
+				entity.FrameState=2;
+			else
+			
+				entity.setAnimationState("switchState", "state6");
+				entity.FrameState=3;
+			end 
+	
+	
+	
+	
+	
+	end
 
-entity.setAnimationState("switchState", "poster3");
-entity.FrameState=3;
-end 
-elseif entity.direction()==-1 then
-if RandomNumber <= 33.3 then
-entity.setAnimationState("switchState", "Lposter1");
 
-elseif RandomNumber<=66.7 then
-entity.setAnimationState("switchState", "Lposter2");
-entity.FrameState=2;
-else
 
-entity.setAnimationState("switchState", "Lposter3");
-entity.FrameState=3;
-end 
+end
+
