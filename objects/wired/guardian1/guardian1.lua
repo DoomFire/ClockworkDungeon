25b6ca10edbe1474e9c996965f3f514484d1b25d
DoomFire <insertuserIDhere@hotmail.com>
2014-03-28 20:40:05 +0800

    Edited the Guardian spawner.
    Gave it a cool description and made the sprite a bit cleaner + updated
    it to the new colour scheme.

--- objects/wired/guardian1/guardian1.lua
+++ objects/wired/guardian1/guardian1.lua
@@ -7,11 +7,11 @@ end
 function onInboundNodeChange(args)
 	entity.smash()
     entity.setAnimationState("beaconState", "active")
-    world.spawnMonster("autoguard", entity.toAbsolutePosition({ -0.7, 2.0 }), { level = 1 })
+    world.spawnMonster("autoguard", entity.toAbsolutePosition({ -0.7, 1.0 }), { level = 1 })
 end
 
 function onInteraction(args)
 	entity.smash()
     entity.setAnimationState("beaconState", "idle")
-    world.spawnMonster("autoguard", entity.toAbsolutePosition({ -0.7, 2.0 }), { level = 1 })
+    world.spawnMonster("autoguard", entity.toAbsolutePosition({ -0.7, 1.0 }), { level = 1 })
 end
