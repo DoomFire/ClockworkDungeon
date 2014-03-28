15d8b60d096121fbcdee61c3b366c045715085f6
DoomFire <insertuserIDhere@hotmail.com>
2014-03-28 22:02:50 +0800

    Changed the sprite and messed with the spawning.

--- objects/wired/guardian1drop/guardian1drop.lua
+++ objects/wired/guardian1drop/guardian1drop.lua
@@ -7,11 +7,11 @@ end
 function onInboundNodeChange(args)
 	--entity.smash()
     entity.setAnimationState("beaconState", "active")
-    world.spawnMonster("autoguard", entity.toAbsolutePosition({ -0.7, -0.5 }), { level = 1 })
+    world.spawnMonster("autoguard", entity.toAbsolutePosition({ -0.7, 1 }), { level = 1 })
 end
 
 function onInteraction(args)
 	--entity.smash()
     entity.setAnimationState("beaconState", "active")
-    world.spawnMonster("autoguard", entity.toAbsolutePosition({ -0.7, -0.5 }), { level = 1 })
+    world.spawnMonster("autoguard", entity.toAbsolutePosition({ -0.7, 1 }), { level = 1 })
 end
