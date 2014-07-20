b32c6dc87956e2d5d308752a961978cbef513865
connery0 <onmymarkburn4321burn@gmail.com>
2014-07-20 16:44:02 +0200

    Centurian pack now hovers
    if the pack is active, hold up to hover

--- tech/centurianPack/centurianPack.lua
+++ tech/centurianPack/centurianPack.lua
@@ -8,6 +8,10 @@ function init()
   data.superJumpTimer = 0
   data.animationDelay = 0.5
   data.deactivating=false
+  data.jumping = false
+  data.hovering = false
+  
+  
 end
 
 function uninit()
@@ -21,18 +25,35 @@ function uninit()
 end
 
 function input(args)
+
   local move = nil
+ 
+-- look if F is pressed
+		--"special" == pressing F
   if args.moves["special"] == 1 and not data.specialLast then
-    if data.active then
-      return "morphballDeactivate"
-    else
-      return "morphballActivate"
+    
+	if data.active then
+    
+		data.hovering = false
+		return "morphballDeactivate"
+    
+	else
+		data.hovering = false
+      return "centurainPackActivate"
     end
 	
-    elseif data.active then
-      if args.moves["jump"] and args.moves["up"] and tech.onGround() then
-        return "superjump"
-	  end
+	
+-- if F isn't pressed then ...	
+	elseif data.active then
+
+		
+		if  args.moves["up"] and not tech.onGround() then 	--		and args.moves["jump"] then-- 
+--			world.logInfo("DoubleJump")
+--			return "superjump"
+			data.hovering = true	
+		else
+			 data.hovering = false
+		end
   end
  
   data.specialLast = args.moves["special"] == 1
@@ -49,7 +70,18 @@ function update(args)
   local ballRadius = tech.parameter("ballRadius")
   local ballFrames = tech.parameter("ballFrames")
 
-  if not data.active and args.actions["morphballActivate"] then
+  
+  
+			if  data.hovering then
+				tech.yControl(0, 600, true)
+				tech.applyMovementParameters({gravityEnabled = false})
+			else
+				tech.applyMovementParameters({gravityEnabled = true})
+			end
+  
+  
+  
+  if not data.active and args.actions["centurainPackActivate"] then
     tech.setAnimationState("morphball", "activate")
     tech.setVisible(true)
 --    tech.translate({0, ballTransformHeightChange})
@@ -64,15 +96,20 @@ function update(args)
 	  data.deactivating=true
   end
 
+  
+
   if data.active then
---    tech.applyMovementParameters(ballCustomMovementParameters)
+
+  
+  
 -- 	  if we have to turn off, then count down the timer
 	if data.deactivating then
 	  if data.animationDelay <= 0 then
       tech.setVisible(false)
---      tech.translate({0, -ballTransformHeightChange})
---      tech.setParentAppearance("normal")
+
+--	  can we use Items while flying?
       tech.setToolUsageSuppressed(false)
+
       data.angle = 0
       data.active = false
 	  data.animationDelay = 0.5
@@ -81,7 +118,21 @@ function update(args)
 	  data.animationDelay = data.animationDelay - args.dt
 	  end
     end
+	
+	
+	-- grounc check
+	
+	
+	
+	if tech.onGround() then
+		data.jumping=false
+	end
+	
+	
 
+	
+	
+	
 	-- Super Jump
   local energyUsage = tech.parameter("energyUsage")
   local superJumpSpeed = tech.parameter("superjumpSpeed")
@@ -91,16 +142,17 @@ function update(args)
 
   local usedEnergy = 0
 
-  if args.actions["superjump"] and tech.onGround() and data.superJumpTimer <= 0 and args.availableEnergy > energyUsage then
-    tech.playImmediateSound(superjumpSound)
-    data.superJumpTimer = superJumpTime
-    usedEnergy = energyUsage
+  if args.actions["superjump"] then-- and tech.onGround() and data.superJumpTimer <= 0 and args.availableEnergy > energyUsage then
+    tech.jump(true)
   end
 
    if data.superJumpTimer > 0 then
     tech.yControl(superJumpSpeed, superJumpControlForce)
     data.superJumpTimer = data.superJumpTimer - args.dt
   end
+  
+
+
   
   if usedEnergy ~= 0 then
     return usedEnergy
